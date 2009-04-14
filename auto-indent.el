717c4b21643ecc8317cb8669d35d6d460bf8c3a4
Caleb Reach <jtxx000@gmail.com>
2009-04-14 21:53:18 +0100

    Fixed a bug in auto-indent/beginning-of-line affecting the last line in a buffer

--- auto-indent.el
+++ auto-indent.el
@@ -10,7 +10,8 @@
   (beginning-of-line)
   (save-match-data
     (if (search-forward-regexp "[^ \t]" nil t)
-        (backward-char))))
+        (backward-char)
+      (indent-according-to-mode))))
 
 (defun auto-indent/point-at-beginning-of-line-text ()
   (<= (point)
