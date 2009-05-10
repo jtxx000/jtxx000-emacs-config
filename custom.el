635c376e240a3ce43617cbef6f2b7a88cfe6b123
Caleb Reach <jtxx000@gmail.com>
2009-05-10 02:33:34 +0100

    Changed font-lock to update instantly

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(jit-lock-context-time 0)
  '(mac-command-modifier (quote alt))
  '(mac-option-modifier (quote meta))
  '(mouse-wheel-progressive-speed nil)
