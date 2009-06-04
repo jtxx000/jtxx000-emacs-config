eeaf4b07dfdb3f484098f797715a036ced6fb742
Caleb Reach <jtxx000@gmail.com>
2009-06-04 11:08:42 +0100

    Reordered hippie-expand-try-functions-list.

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(hippie-expand-try-functions-list (quote (try-complete-file-name-partially try-complete-file-name try-expand-all-abbrevs try-expand-list try-expand-dabbrev try-expand-dabbrev-all-buffers try-expand-dabbrev-from-kill try-expand-line try-complete-lisp-symbol-partially try-complete-lisp-symbol)))
  '(jit-lock-context-time 0)
  '(mac-command-modifier (quote alt))
  '(mac-option-modifier (quote meta))
