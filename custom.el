29a3427b04d2e60aca05e5a142c7a8c20f232885
Caleb Reach <jtxx000@gmail.com>
2009-06-04 11:07:22 +0100

    Enabled transient-mark mode.

--- custom.el
+++ custom.el
@@ -10,7 +10,7 @@
  '(mouse-wheel-scroll-amount (quote (5 ((shift) . 1) ((control)))))
  '(python-indent 3)
  '(rst-adornment-faces-alist (quote ((t . font-lock-keyword-face) (nil . font-lock-keyword-face) (1 . font-lock-function-name-face) (2 . font-lock-function-name-face) (3 . font-lock-function-name-face) (4 . font-lock-function-name-face) (5 . font-lock-function-name-face) (6 . font-lock-function-name-face))))
- '(transient-mark-mode nil))
+ '(transient-mark-mode t))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
