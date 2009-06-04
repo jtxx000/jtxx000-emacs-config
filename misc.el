407c437adef46ba97085ff44642746ad96f609ca
Caleb Reach <jtxx000@gmail.com>
2009-06-04 11:33:43 +0100

    Enabled narrow-to-region and set-goal-column.

--- misc.el
+++ misc.el
@@ -2,3 +2,6 @@
 (load custom-file 'noerror)
 (setq auto-save-list-file-prefix "~/.emacs-meta/auto-save-list/.saves-")
 (setq backup-directory-alist '(("." . "~/.emacs-meta/")))
+
+(put 'narrow-to-region 'disabled nil)
+(put 'set-goal-column 'disabled nil)
