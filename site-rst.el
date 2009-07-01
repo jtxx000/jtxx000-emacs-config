2c1b199ca51012e2a1e7bc3428b27be1ece2c3cd
Caleb Reach <jtxx000@gmail.com>
2009-07-01 13:22:58 +0100

    Changed paragraph separators for rst-mode to standard values.

--- site-rst.el
+++ site-rst.el
@@ -3,6 +3,8 @@
 (defun site-init-rst ()
   (define-key rst-mode-map (kbd "C-=") 'rst-adjust-decoration)
   (auto-fill-mode)
+  (setq paragraph-start "\\f\\|[ \\t]*$")
+  (setq paragraph-separate "[ \\t\\f]*$")
   (flyspell-mode))
 
 (add-hook 'rst-mode-hook 'site-init-rst)
