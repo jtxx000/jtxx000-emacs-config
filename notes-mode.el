8f751a57f94ec86de88b1fdbce54666d742899ab
Caleb Reach <jtxx000@gmail.com>
2009-07-01 13:20:48 +0100

    Added notes-mode-insert-section.

--- notes-mode.el
+++ notes-mode.el
@@ -2,7 +2,13 @@
 
 (add-to-list 'magic-mode-alist (cons notes-mode-section-regexp 'notes-mode))
 
+(defun notes-mode-insert-section ()
+  (interactive)
+  (insert "== ")
+  (save-excursion (insert " ==")))
+
 (define-derived-mode notes-mode text-mode "Notes"
+  (define-key notes-mode-map (kbd "C-=") 'notes-mode-insert-section)
   (setq font-lock-defaults
         `(((,notes-mode-section-regexp . font-lock-function-name-face)
            ("^\\(.*\\) - " 1 font-lock-type-face)
