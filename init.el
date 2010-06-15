044314571dc73e9ca28f38af9a44a124e6a704d7
Caleb Reach <jtxx000@gmail.com>
2010-06-15 15:56:15 -0400

    added m$ modes

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 (add-to-list 'load-path "~/.emacs.d")
 (add-to-list 'load-path "~/.emacs.d/vendor")
 
-(server-start)
+;(server-start)
 
 (load-library "visual")
 (load-library "util")
@@ -15,8 +15,8 @@
 (load "haskell-mode-2.4/haskell-site-file.el")
 (autoload 'cycle-buffer "cycle-buffer" "Cycle forward." t)
 (require 'anything-config)
-(load "auctex.el" nil t t)
-(load "preview-latex.el" nil t t)
+;(load "auctex.el" nil t t)
+;(load "preview-latex.el" nil t t)
 (load-library "site-yasnippet")
 (load-library "site-rst")
 (load-library "site-io")
@@ -34,8 +34,15 @@
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'post-forward-angle-brackets)
 
+(autoload 'csharp-mode "csharp-mode" "Major mode for editing C# code." t)
 (autoload 'd-mode "d-mode" "Major mode for editing D code." t)
+(require 'vbnet-mode)
+
 (add-to-list 'auto-mode-alist '("\\.d[i]?\\'" . d-mode))
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 (add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\work-hours\\'" . time-mode))
+(add-to-list 'auto-mode-alist '("\\.cs$" . csharp-mode))
+(add-to-list 'auto-mode-alist '("\\.vb$" . vbnet-mode))
+
+(setq auto-mode-alist (append '() auto-mode-alist))
